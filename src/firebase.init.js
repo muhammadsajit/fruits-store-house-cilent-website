11a7b1f0990f5cb07d56422917d2f2b1d434a80a
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-06 15:38:59 +0600

    firebase.init file added

---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgGpaxpmvHkJ4eWhAH_0lX7xYUsOmZ6DQ",
+  authDomain: "fruits-store-house.firebaseapp.com",
+  projectId: "fruits-store-house",
+  storageBucket: "fruits-store-house.appspot.com",
+  messagingSenderId: "544401191617",
+  appId: "1:544401191617:web:a6cc707368d4c60886da40"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth =getAuth (app);
+export default auth;
