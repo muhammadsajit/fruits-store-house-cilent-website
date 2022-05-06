6c8fad80e220fd4baaacef83b0d6a3ea71d89a2f
muhammadsajit <sajit.du.niter@gmail.com>
2022-05-06 10:45:06 +0600

    banner added

--- src/PagesOfWebsite/Home/Banner/Banner.js
+++ src/PagesOfWebsite/Home/Banner/Banner.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import fruit1 from '../../../images/fruits1.png';
+import fruit2 from '../../../images/fruit2.png';
+import fruit3 from '../../../images/fruit3.png'
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
     return (
         <div>
-            <h1>Banner page</h1>
+           
+           <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          style={{height:'500px'}}
+          src={fruit1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Fruits Store1 </h3>
+          <p>In Fruit Store,many fruits are store in it</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          style={{height:'500px'}}
+          src={fruit2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Fruits Store2</h3>
+          <p>Fruits store in refrigerator</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          style={{height:'500px'}}
+          src={fruit3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Fruits Store3</h3>
+          <p>
+            In fruit store fruits are reserved then fruits are divered people;
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
         </div>
     );
 };
--- src/PagesOfWebsite/Home/Home.js
+++ src/PagesOfWebsite/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Banner from './Banner/Banner';
 import Header from './Header/Header';
 
 const Home = () => {
@@ -6,6 +7,8 @@ const Home = () => {
         <div>
             
             <Header></Header>
+            <h2 className='text-center text-primary'>Fruits Store House</h2>
+            <Banner></Banner>
         </div>
     );
 };
